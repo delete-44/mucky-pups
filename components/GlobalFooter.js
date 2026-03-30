a511c0d327c0a4f02d7bddca4da18bf69cd3efab
delete-44 <delete44+tech@protonmail.com>
2026-03-30 15:21:41 +0100

    Footer component

---
+++ components/GlobalFooter.js
@@ -0,0 +1,37 @@
+// Minimal + lightweight footer component; no shadow DOM, no interactivity,
+// just lets us reuse the nav header.
+class GlobalFooter extends HTMLElement {
+  static define(tag = "global-footer") {
+    customElements.define(tag, this);
+  }
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <footer>
+        <div class="footer-content display-flex gap-m">
+          <ul>
+            <li><a href="/#about">About</a></li>
+            <li><a href="/#contact">Contact</a></li>
+            <li><a href="/#dress-code">Dress Code</a></li>
+            <li><a href="/#code-of-conduct">Code of Conduct</a></li>
+          </ul>
+          <ul>
+            <!-- TODO -->
+            <li><a>Gallery</a></li>
+            <li><a href="/join">Become a Member</a></li>
+          </ul>
+        </div>
+      </footer>
+    `;
+  }
+}
+
+GlobalFooter.define();
